1969cff45109e7db9bb9d07b6d9e0032664f2efd
gauravgoyal112 <gauravgoyal112@gmail.com>
2017-01-11 09:49:02 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/batch/Application.java
@@ -0,0 +1,16 @@
+package com.nagarro.batch;
+
+/**
+ * Created by gauravgoyal on 1/6/2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/nagarro/batch/Person.java
@@ -0,0 +1,42 @@
+package com.nagarro.batch;
+
+/**
+ * Created by gauravgoyal on 1/6/2017.
+ */
+
+
+public class Person {
+    private String lastName;
+    private String firstName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
+    }
+
+}
